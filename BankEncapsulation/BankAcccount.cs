aa9828937056c25a16ae259791b6b4d0565e26ec
Katherine Vail <jabberduckygame@gmail.com>
2023-05-23 11:39:27 -0500

    Completed exercise.

---
+++ BankEncapsulation/BankAcccount.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAcccount
+    {
+        private double _balance = 0;
+
+        public void Deposit(double moneyAdded)
+        {
+            _balance += moneyAdded;
+        }
+
+        public double GetBalance()
+        {
+            return _balance;
+        }
+
+    }
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,50 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var myAccount = new BankAcccount();
+
+            Console.WriteLine("Your current balance is:");
+            var balanceTotal = myAccount.GetBalance();
+            Console.WriteLine(balanceTotal);
+
+            Console.WriteLine();
+
+            Console.WriteLine("How much would you like to add?");
+            double total = Convert.ToDouble(Console.ReadLine());
+            myAccount.Deposit(total);
+            balanceTotal = myAccount.GetBalance();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Your current balance now is:");
+            Console.WriteLine(balanceTotal);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Would you like to deposit more?");
+            string answer = Console.ReadLine().ToLower();
+
+            Console.WriteLine();
+
+            if (answer == "yes")
+            {
+                Console.WriteLine("How much would you like to add?");
+                double total2 = Convert.ToDouble(Console.ReadLine());
+                myAccount.Deposit(total2);
+                balanceTotal = myAccount.GetBalance();
+
+                Console.WriteLine();
+
+                Console.WriteLine("Your current balance now is:");
+                Console.WriteLine(balanceTotal);
+            }
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine("Thank you have a great day!");
+            }
+          
+
         }
     }
 }
